dbeb2648a53c01169d2771426348129fcd583e53
Dylan Beattie <dylan@dylanbeattie.net>
2020-04-26 10:06:07 +0100

    Added Car database with Prius and BMW

--- autoweb/Autoweb.Website/Controllers/CarsController.cs
+++ autoweb/Autoweb.Website/Controllers/CarsController.cs
@@ -6,18 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Autoweb.Website.Models;
+using Autoweb.Website.Data;
 
 namespace Autoweb.Website.Controllers {
 	public class CarsController : Controller {
+		private readonly CarDatabase db;
 		private readonly ILogger<HomeController> _logger;
 
-		public CarsController(ILogger<HomeController> logger) {
+		public CarsController(ILogger<HomeController> logger, CarDatabase db) {
+            this.db = db;
 			_logger = logger;
 		}
 
 		public IActionResult Index() {
-            
-			return View();
+            var cars = db.Cars;
+			return View(cars);
 		}
 	}
 }
--- autoweb/Autoweb.Website/Data/CarDatabase.cs
+++ autoweb/Autoweb.Website/Data/CarDatabase.cs
@@ -10,11 +10,22 @@ namespace Autoweb.Website.Data {
                     Make = "Toyota",
                     Name = "Prius",                
                 };
+                var bmw5 = new CarModel {
+                    Make = "BMW",
+                    Name = "5 Series"
+                };
+
 				yield return new Car { 
                     Model = prius,
                     Color = "Blue",
                     RegistrationNumber = "AB1234CD",
                     Year = 2016
+                };
+                yield return new Car {
+                    Model = bmw5,
+                    Color = "Black",
+                    RegistrationNumber = "ABC123",
+                    Year = 2009                    
                 };
 			}
 		}
