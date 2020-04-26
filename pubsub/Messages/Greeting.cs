9ad002510e2bcdcf0d0a1ca6178e6146167ef018
Dylan Beattie <dylan@dylanbeattie.net>
2020-04-26 12:30:03 +0100

    EasyNetQ Publisher example

---
+++ pubsub/Messages/Greeting.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Messages {
+    public class Greeting {
+        public string Sender { get; set;  }
+        public string Content { get; set;  }
+    }
+}
--- pubsub/Publisher/Program.cs
+++ pubsub/Publisher/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using EasyNetQ;
+using Messages;
 
-namespace Publisher
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Publisher {
+    class Program {
+        static void Main(string[] args) {
+
+            var bus = RabbitHutch.CreateBus("amqp://lplnqgia:AGXzy06s5Pt7Z9zgPJshyNofOqRES5-h@roedeer.rmq.cloudamqp.com/lplnqgia");
+            Console.WriteLine("Connected! Ready to send messages!");
+            Console.Write("Enter your name:" );
+            var name = Console.ReadLine();
+            while (true) {
+                Console.Write("Enter a message: ");
+
+                var messageContent = Console.ReadLine();
+                var message = new Greeting {
+                    Sender = name,
+                    Content = messageContent
+                };
+                bus.Publish(message);
+            }
         }
     }
 }
