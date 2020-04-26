4c94e5bd4ba45ca93a976ca1a2d35c8b347cb9ff
Dylan Beattie <dylan@dylanbeattie.net>
2020-04-26 12:34:39 +0100

    Added Subscriber example

--- pubsub/Subscriber/Program.cs
+++ pubsub/Subscriber/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using EasyNetQ;
+using Messages;
 
-namespace Subscriber
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Subscriber {
+    class Program {
+        private static string amqp =
+            "amqp://lplnqgia:AGXzy06s5Pt7Z9zgPJshyNofOqRES5-h@roedeer.rmq.cloudamqp.com/lplnqgia";
+        static void Main(string[] args) {
+            var bus = RabbitHutch.CreateBus(amqp);
+            bus.Subscribe<Greeting>("dylan", HandleGreeting);
+        }
+
+        private static void HandleGreeting(Greeting g) {
+            Console.WriteLine($"{g.Sender} : {g.Content}");
         }
     }
 }
